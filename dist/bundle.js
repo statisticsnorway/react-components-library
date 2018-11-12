4bfe54bc0ad181629172f7eba835f98f08b08b00
mariannemellem <35998802+mariannemellem@users.noreply.github.com>
2018-11-12 12:18:13 +0100

    Bibliotek (#3)
    * Added rollup and babel to build library.
    Added core functionality for data fetching.

---
+++ dist/bundle.js
@@ -0,0 +1,20 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'default' in ex) ? ex['default'] : ex; }
+
+require('react');
+var axios = _interopDefault(require('axios'));
+
+function dataFetcher(url) {
+  return new Promise(function (resolve, reject) {
+    axios.get(url).then(function (response) {
+      resolve(response.data);
+    }).catch(function (error) {
+      reject(error);
+    });
+  });
+}
+
+exports.dataFetcher = dataFetcher;
--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import { dataFetcher } from './DataFetcher'
+
+const lds = 'http://localhost:9090/data/AgentInRole/'
 
 class App extends Component {
+
+  componentDidMount(){
+    dataFetcher(lds).then((result) => console.log('Result from url ' + lds + ': ',result)).catch((reason) => console.log('Error: ', reason))
+  }
+
   render () {
     return (
-      null
-    );
+      <div>
+        <form>
+          <label> Test av dc-react-component-library </label>
+        </form>
+      </div>
+    )
   }
 }
-
-export default App;
+export default App
