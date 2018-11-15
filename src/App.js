b9cbf9b100aad5d245ae1d78c33d9eb42ba1220f
mariannemellem <35998802+mariannemellem@users.noreply.github.com>
2018-11-15 13:29:27 +0100

    Data saver (#4)
    * Added core functionality for data saving.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,59 @@
 import React, { Component } from 'react'
 import { dataFetcher } from './DataFetcher'
-
-const lds = 'http://localhost:9090/data/AgentInRole/'
+import { dataSaver } from './DataSaver'
+import { Form, Segment, Header, Divider, Button } from 'semantic-ui-react'
 
 class App extends Component {
 
-  componentDidMount(){
-    dataFetcher(lds).then((result) => console.log('Result from url ' + lds + ': ',result)).catch((reason) => console.log('Error: ', reason))
+  handleOnClickFetch () {
+    const ldsFetchUrl = 'http://localhost:9090/data/Agent/'
+
+    dataFetcher(ldsFetchUrl).then((result) => console.log('Result from url ' + ldsFetchUrl + ': ', result)).catch((reason) => console.log('Error: ', reason))
+  }
+
+  handleOnClickSave () {
+    const ldsSaveUrl = 'http://localhost:9090/data/Agent/b02e7d00-e740-11e8-9e27-758293bd596e'
+
+    const data = {
+      "id": "b02e7d00-e740-11e8-9e27-758293bd596e",
+      "name":
+        [{"languageCode": "nb",
+          "languageText": "c"}],
+      "description":
+        [{"languageCode": "nb",
+          "languageText": "c"}],
+      "createdDate": "2018-11-13T12:36:53.092Z",
+      "createdBy": "Test",
+      "version": "1.0.0",
+      "versionValidFrom": "2018-11-13T12:36:53.093Z",
+      "lastUpdatedDate": "2018-11-13T12:36:53.093Z",
+      "lastUpdatedBy": "Test",
+      "validFrom": "2018-11-13T12:36:53.093Z",
+      "validUntil": "2019-11-13T12:36:53.093Z",
+      "agentType": "ORGANIZATION",
+      "isExternal": false
+    }
+
+    dataSaver(ldsSaveUrl, data).then((result) => console.log('Result from url ' + ldsSaveUrl + ': ', result)).catch((reason) => console.log('Error: ', reason))
   }
 
   render () {
     return (
       <div>
-        <form>
-          <label> Test av dc-react-component-library </label>
-        </form>
+        <Form>
+          <Segment basic textAlign='center'>
+          <Header as='h1' dividing>
+            Test av dc-react-component-library
+          </Header>
+          <Divider/>
+          <Button onClick={this.handleOnClickFetch}>Fetch</Button>
+          <Divider/>
+          <Button onClick={this.handleOnClickSave}>Save</Button>
+          </Segment>
+        </Form>
       </div>
     )
   }
 }
+
 export default App
--- src/main.js
+++ src/main.js
@@ -1,1 +1,2 @@
 export { dataFetcher } from './DataFetcher'
+export { dataSaver } from './DataSaver'
