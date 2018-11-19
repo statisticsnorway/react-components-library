4efe06eef167a208fbe3389c025928dc6f93a5d7
Rupinder Kaur <Rupinder.Kaur@ssb.no>
2018-11-19 09:43:07 +0100

    Schema handler (#6)
    * Merge default UI schema and UI schema with Json Schema. Display results in Code-Mirror for verifying

--- src/App.js
+++ src/App.js
@@ -3,16 +3,40 @@ import { dataFetcher } from './DataFetcher'
 import { dataSaver } from './DataSaver'
 import { dataDeleter } from './DataDeleter'
 import { Form, Segment, Header, Divider, Button } from 'semantic-ui-react'
+import JsonSchemaHandler from './schemaHandler/JsonSchemaHandler'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      hideSchemaHandler: true
+    }
 
-  handleOnClickFetch () {
+    this.activateSchemasHandling = this.activateSchemasHandling.bind(this)
+    this.deactivateSchemasHandling = this.deactivateSchemasHandling.bind(this)
+  }
+
+  activateSchemasHandling() {
+    this.setState({
+      ...this.state,
+      hideSchemaHandler: false
+    })
+  }
+
+  deactivateSchemasHandling() {
+    this.setState({
+      ...this.state,
+      hideSchemaHandler: true
+    })
+  }
+
+  handleOnClickFetch() {
     const ldsFetchUrl = 'http://localhost:9090/data/Agent/'
 
     dataFetcher(ldsFetchUrl).then((result) => console.log('Result from url ' + ldsFetchUrl + ': ', result)).catch((reason) => console.log('Error: ', reason))
   }
 
-  handleOnClickSave () {
+  handleOnClickSave() {
     const ldsSaveUrl = 'http://localhost:9090/data/Agent/b02e7d00-e740-11e8-9e27-758293bd596e'
 
     const data = {
@@ -55,6 +79,20 @@ class App extends Component {
       <div>
         <Form>
           <Segment basic textAlign='center'>
+            <Header as='h1' dividing>
+              Test av dc-react-component-library
+            </Header>
+            <Divider />
+            <Button onClick={this.handleOnClickFetch}>Fetch</Button>
+            <Divider />
+            <Button onClick={this.handleOnClickSave}>Save</Button>
+          </Segment>
+          <Segment basic textAlign='center'>
+            <Button basic onClick={this.activateSchemasHandling}>Handle Schemas</Button>
+            <Button basic onClick={this.deactivateSchemasHandling}>Cancel</Button>
+            <Segment textAlign='left' name="jsonHandler" hidden={this.state.hideSchemaHandler}>
+              <JsonSchemaHandler />
+            </Segment>
             <Header as='h1' dividing>
               Test av dc-react-component-library
             </Header>
